2b433d1c1fa1cea9d182d8751bca44f39a66a246
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-04-13 14:26:16 +0200

    Api calls in progress

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { actuatorReducer as services } from './ActuatorReducer';
 import { routingReducer as location } from './RoutingReducer';
 import { configurationReducer as configuration } from './ConfigurationReducer';
+import { apiCallsReducer as apiCallsInProgress } from './ApiCallsReducer';
 
 const rootReducer = combineReducers({
+    apiCallsInProgress,
     configuration,
     location,
     services,
