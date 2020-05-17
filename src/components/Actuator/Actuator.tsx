9a6def064b9cc04697c2d493da89b08747e2f77c
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-05-17 19:39:07 +0200

    Panels

--- src/components/Actuator/Actuator.tsx
+++ src/components/Actuator/Actuator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Blinker from '../Blinker/Blinker';
-import { Typography } from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography } from '@material-ui/core';
 import styles from './Actuator.module.scss';
 import { ControlActions, IService } from '../../redux/reducers/Types';
 import { SlideInDiv } from '../AnimationComponents/SlideInDiv';
@@ -18,19 +18,24 @@ export const Actuator: React.FC<IActuatorProps> = ({ service, isApiCallInProgres
 
     return (
         <SlideInDiv childrenClassName={styles.Actuator}>
-            <Blinker status={status}/>
-            <div>
-                {service.icon && <img src={service.icon}/>}
-            </div>
+            <ExpansionPanel className={styles.ControlsContainer}>
+                <ExpansionPanelSummary>
+                    <Blinker status={status}/>
+                    <div>
+                        {service.icon && <img src={service.icon}/>}
+                    </div>
 
-            <div>
-                <Typography variant="h2">{service.name}</Typography>
-                <small className={styles.ActuatorSubtitle}>
-                    Service address - <a href={serviceUrl}>{serviceUrl}</a>
-                </small>
-            </div>
-
-            {/*<Controls {...{ isApiCallInProgress, status, onControlButtonClick}}/>*/}
+                    <div>
+                        <Typography variant="h2">{service.name}</Typography>
+                        <small className={styles.ActuatorSubtitle}>
+                            Service address - <a href={serviceUrl}>{serviceUrl}</a>
+                        </small>
+                    </div>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Controls {...{ isApiCallInProgress, status, onControlButtonClick }}/>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
         </SlideInDiv>
     );
 };
